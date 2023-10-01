23da944dbe8c29dbdce2d47eea355ea47c092c6b
Peter Matev <peter@wayve.ai>
2023-10-01 11:59:35 +0100

    add protobuf types for messages

---
+++ src/compile_protos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Generate TypeScript bindings
+protoc --plugin=protoc-gen-ts=frontend/node_modules/.bin/protoc-gen-ts \
+       --js_out=import_style=commonjs,binary:frontend/ \
+       --ts_out=frontend/ \
+       proto/*.proto
+
+# Generate Python bindings
+protoc --python_out=viewport/ \
+       proto/*.proto
